4c713040e847a4a2c5d1de7f2fbc10d0fbfd2b94
Thomas Hancock <thomas.hancock@hpe.com>
2021-03-24 11:47:48 +0000

    Added CL argument reading

--- app/Main.hs
+++ app/Main.hs
@@ -1,8 +1,20 @@
 module Main where
 
-import Lib
+import System.Environment
 
 import DatabaseLocal (query)
 
-main :: IO ()
-main = query 2
+-- main :: IO ()
+-- main = query 2
+
+procArgs :: [String] -> String
+procArgs [] = "No args provided"
+procArgs (x:xs)
+  | x == "Add" = "Add requested"
+  | x == "List" = "List requested"
+  | otherwise = "Invalid command"
+
+main = do
+  args <- getArgs
+  let result = procArgs args
+  putStrLn result
