7423c75d57a05bdef56311b94a7cfa5f576d33eb
Thomas Hancock <thomas.hancock@hpe.com>
2021-03-24 12:04:10 +0000

    Added placeholder functions for add and list functionality

--- app/Main.hs
+++ app/Main.hs
@@ -1,17 +1,30 @@
 module Main where
 
 import System.Environment
+import Data.List (unwords)
 
 import DatabaseLocal (query)
 
 -- main :: IO ()
 -- main = query 2
 
+argConcat :: [String] -> String
+argConcat [] = ""
+argConcat x = unwords x
+
+add :: [String] -> String
+add [] = "Add requested but no args provided"
+add x = "Add requested with args: " ++ argConcat x
+
+list :: [String] -> String
+list [] = "List requested but no args provided"
+list x = "List requested with args: " ++ argConcat x
+
 procArgs :: [String] -> String
 procArgs [] = "No args provided"
 procArgs (x:xs)
-  | x == "Add" = "Add requested"
-  | x == "List" = "List requested"
+  | x == "Add" = add xs
+  | x == "List" = list xs
   | otherwise = "Invalid command: " ++ x
 
 main = do
