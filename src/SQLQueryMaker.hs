a34cfa3f3450660869a4585f4da03c770c51a2bf
Thomas Hancock <thomas.hancock@hpe.com>
2021-03-25 14:44:21 +0000

    Added and refactored SQL Query Maker

--- src/SQLQueryMaker.hs
+++ src/SQLQueryMaker.hs
@@ -1,34 +1,40 @@
 module SQLQueryMaker (
-  makeSelect
+  makeSelect,
+  getArgs
 ) where
 
--- afterExtra = "WHERE"
-
-addExtra :: [String] -> String
-addExtra [] = ""
-addExtra x
+-- Constructes the WHERE SQL section based on the arguments 
+makeWhere :: [String] -> String
+makeWhere [] = ""
+makeWhere x
     | "after" `elem` x && "before" `elem` x = "WHERE (date <= ? AND date >= ?)"
     | "after" `elem` x = "WHERE date >= ?"
     | "before" `elem` x = "WHERE date <= ?"
     | otherwise = ""
 
-commands :: [a] -> [a]
-commands [] = []
-commands (x:xs) = x:values xs
+-- Extracts the keys from the arguments
+keys :: [a] -> [a]
+keys [] = []
+keys (x:xs) = x:values xs
 
+-- Extracts the values from the arguments
 values :: [a] -> [a]
 values [] = []
-values (x:xs) = commands xs
+values (x:xs) = keys xs
 
-listToTuple2 :: [a] -> (a,a)
-listToTuple2 [x, y] = (x, y)
+-- Returns the start of the SQL select command
+selectStart :: [Char]
+selectStart = "SELECT * FROM test"
 
-start :: [Char]
-start = "SELECT * FROM test"
+-- Returns the end of the SQL select command
+selectEnd :: [Char]
+selectEnd = "ORDER BY date, desc"
 
-end :: [Char]
-end = "ORDER BY date, desc"
-
+-- Constructs an SQL select command from passed arguments
 makeSelect :: [String] -> String
-makeSelect [] = start ++ " " ++ end
-makeSelect x = start ++ " " ++ addExtra (commands x) ++ " " ++ end
+makeSelect [] = selectStart ++ " " ++ selectEnd
+makeSelect x = selectStart ++ " " ++ makeWhere (keys x) ++ " " ++ selectEnd
+
+-- Extracts the values from the passed arguments
+getArgs :: [String] -> [String]
+getArgs = values
